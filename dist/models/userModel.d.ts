10573da2b9c05551686c1ceb2202b9e8d4b04035
Akindele Simeon <adisaakindele2@gmail.com>
2025-07-02 22:50:05 +0100

    save

--- dist/models/userModel.d.ts
+++ dist/models/userModel.d.ts
@@ -3,6 +3,10 @@ export interface IUser extends Document {
     email: string;
     password: string;
 }
-export declare const User: mongoose.Model<IUser, {}, {}, {}, any>;
+export declare const User: mongoose.Model<IUser, {}, {}, {}, mongoose.Document<unknown, {}, IUser, {}> & IUser & Required<{
+    _id: unknown;
+}> & {
+    __v: number;
+}, any>;
 export declare const connectDB: () => Promise<void>;
 //# sourceMappingURL=userModel.d.ts.map
